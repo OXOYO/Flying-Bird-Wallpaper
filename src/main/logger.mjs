5556cae6a4b1b6ae4a176b7802edbfd1d8b2184e
OXOYO <zmn2007.hi@163.com>
2025-05-27 13:41:32 +0800

    feat: 优化日志输出

--- src/main/logger.mjs
+++ src/main/logger.mjs
@@ -1,21 +1,33 @@
 import { join } from 'path'
 import pino from 'pino'
+import { isDev } from './utils/utils.mjs'
 
 export default (logDir, fileName = 'app.log') => {
   // 获取用户数据目录
   const logFilePath = join(logDir || process.env.FBW_LOGS_PATH, fileName)
 
   // 配置 pino-roll 传输
-  const transport = pino.transport({
-    target: 'pino-roll',
-    options: {
-      file: logFilePath, // 日志文件路径
-      frequency: 'daily', // 每天生成一个新的日志文件
-      size: '10M', // 每个日志文件的最大大小
-      mkdir: true, // 自动创建目录
-      dateFormat: 'yyyy-MM-dd' // 自定义日志文件名称，eg: app.log.2025-02-27.1
-    }
-  })
+  const transport = pino.transport(
+    isDev()
+      ? {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname'
+          }
+        }
+      : {
+          target: 'pino-roll',
+          options: {
+            file: logFilePath, // 日志文件路径
+            frequency: 'daily', // 每天生成一个新的日志文件
+            size: '10M', // 每个日志文件的最大大小
+            mkdir: true, // 自动创建目录
+            dateFormat: 'yyyy-MM-dd' // 自定义日志文件名称，eg: app.log.2025-02-27.1
+          }
+        }
+  )
 
   // 创建 Pino 实例
   const logger = pino(transport)
